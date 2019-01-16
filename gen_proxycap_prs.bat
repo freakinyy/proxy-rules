62c3c0393157545f56169555157fc9d64f9c1503
Yu Yao <freakinyy@gmail.com>
2019-01-16 11:07:38 +0800

    ''

--- gen_proxycap_prs.bat
+++ gen_proxycap_prs.bat
@@ -1,7 +1,8 @@
+@echo off
 echo "Generating XML file..."
-.\gen_proxycap_xml.sh -o  .\default.xml
+gen_proxycap_xml.sh -o  default.xml
 echo "Generating PRS file..."
-.\xml2prs.exe .\default.xml .\default.prs
+xml2prs.exe default.xml default.prs
 echo "Updating to Git..."
 git commit -a -m '' --allow-empty-message
 git push
