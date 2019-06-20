1cbc29c3894dd21d2a042fb173bb0484d5674e36
freakinyy <freakinyy@gmail.com>
2019-06-20 10:50:21 +0800

    --allow-empty-message

--- gen_proxycap_xml.sh
+++ gen_proxycap_xml.sh
@@ -104,6 +104,7 @@ cat >> $OUT_TMP_FILE <<EOF
       <programs>
         <program path="Shadowsocks.exe" dir_included="false" />
         <program path="obfs-local.exe" dir_included="false" />
+        <program path="v2ray-plugin.exe" dir_included="false" />
         <program path="BitComet.exe" dir_included="false" />
       </programs>
     </routing_rule>
